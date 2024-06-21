bb1ca7d655dd7de285e31495477e8555833e80d2
juanfernandezzaragoza <juanfernandezzaragoza@hotmail.com>
2024-06-21 15:05:39 -0300

    genetico

---
+++ src/genetic.py
@@ -0,0 +1,203 @@
+from genome import Genome, World
+from ultimatum import Ultimatum
+import numpy as np
+import random
+
+def apply_neural_network_propose(agente, vecino, genome):
+    input = []
+    hist_propuesto = agente.hist_propuesto[vecino.id]
+    ultimos_3 = hist_propuesto[-3:]
+    for i in range(len(ultimos_3)):
+        input.append(ultimos_3[i][0])
+        input.append(ultimos_3[i][1])
+    result_softmax = genome.apply_network(input)
+    chosen_index = random.choices(range(len(result_softmax)), weights=result_softmax, k=1)[0]
+    return chosen_index + 1
+
+def apply_neural_network_accept(agente, vecino, valor, genome):
+    input = []
+    input.append(valor)
+    hist_recibido = agente.hist_recibido[vecino.id]
+    ultimos_3 = hist_recibido[-3:]
+    for i in range(len(ultimos_3)):
+        input.append(ultimos_3[i][0])
+        input.append(ultimos_3[i][1])
+    result_softmax = genome.apply_network(input)
+    # print(result_softmax)
+    chosen_index = random.choices(range(len(result_softmax)), weights=result_softmax, k=1)[0]
+    if chosen_index == 0:
+        return True 
+    else:
+        return False
+
+def strategy_propose(genome):
+    strategy = lambda agente, vecino : apply_neural_network_propose(agente, vecino, genome)
+    return strategy
+
+def strategy_accept(genome):
+    strategy = lambda agente, vecino, valor : apply_neural_network_accept(agente, vecino, valor, genome)
+    return strategy
+
+class Genetico:
+    def __init__(self, n_turnos_por_generacion, n_generaciones, genomas, n_agentes, topologia, mutaciones):
+        self.n_turnos_por_generacion = n_turnos_por_generacion
+        self.n_generaciones = n_generaciones
+        self.genomas = genomas
+        self.fitness_genomas = []
+        self.n_agentes = n_agentes
+        self.topologia = topologia
+        self.mutaciones = mutaciones
+        
+
+        """
+        GENOMAS
+        Los genomas son un array que contiene los genomas de cada agente según su id.
+        Cada elemento es una tupla (genoma_proponer, genoma_aceptar)
+        Ordenamos los genomas al azar antes de asignarlos a los agentes.
+        
+        MUTACIONES
+        Las mutaciones son un diccionario que describe las propiedades de la reproducción. Contiene:
+        {
+            n_sobrevivientes -> el numero de agentes más exitosos que pasan a la siguiente generación.
+            n_mutar_pesos ->  el porcentaje de agnetes que surge de mutar los pesos de la red neuronal.
+            p_mutar_peso -> la probabilidad de mutar el peso de cada nodo en los agentes a los que les mutamos los pesos
+            p_gaussiana -> la probabilidad de que, al mutar los pesos, se haga con distribución gaussiana (cambiar poco los pesos) en lugar de uniforme (cambiar mucho los pesos) -> he usado 0.8
+            n_nuevo_nodo -> numero de agentes a los que se añade un nodo nuevo
+            n_nuevo_eje -> numero de agentes a los que se añade una nueva conexión
+            n_cruces -> numero de agentes que surge de cruzar dos genomas al azar
+            
+            speciation -> booleano que determina si hay especiación
+            c1_distance -> parámetro de especiación
+            c2_distance -> parámetro de especiación
+            c3_distance -> parámetro de especiación
+            cota_especies -> parámetro de especiación
+        }
+        
+        """
+
+
+    def setup_generacion(self, genomas):
+        ultimatum = Ultimatum(self.topologia, self.n_agentes)
+
+        agentes = ultimatum.agentes
+
+        # Asignar al agente i el genoma i.
+        for i in range(len(agentes)):
+            agentes[i].estrategia_proponer = strategy_propose(genomas[i][0])
+            agentes[i].estrategia_aceptar = strategy_accept(genomas[i][1])
+
+        return ultimatum
+    
+    def generacion(self, genomas):
+        # toma una lista de genomas
+        # devuelve el fitness calculado de cada genoma
+
+        ultimatum = self.setup_generacion(genomas)
+
+        for i in range(self.n_turnos_por_generacion):
+            ultimatum.turno()
+
+        ultimatum.calculate_fitness()
+
+        self.fitness_genomas = []
+
+        for i in range(len(genomas)):
+            # Por ahora, vamos sin especiación.
+            self.fitness_genomas.append(ultimatum.agentes[i].media_negociaciones)
+            # Cuando calcule el genetic distance, uso la suma de ambos, no? Sí, total es una combinación lineal de distancias.
+
+    def reproduccion(self, mutaciones):
+        # Ordenar genomas por fitness
+        genomas = self.genomas
+        fitness_genomas = self.fitness_genomas 
+        
+        # Combine genomas and fitness_genomas into a list of tuples (genoma, fitness)
+        combined = list(zip(genomas, fitness_genomas))
+
+        # Sort the combined list based on the fitness value, in descending order
+        sorted_combined = sorted(combined, key=lambda x: x[1], reverse=True)
+        
+        # Extract the sorted genomas
+        sorted_genomas = [genoma for genoma, fitness in sorted_combined]
+        sorted_fitness = [fitness for genoma, fitness in sorted_combined]
+
+        # Update the original arrays if needed
+        genomas = sorted_genomas
+        # print(f"El fitness del primero es {sorted_fitness[0]}")
+        # print(f"El fitness del treinta es {sorted_fitness[29]}")
+        # print(f"El fitness del sesenta es {sorted_fitness[59]}")
+        
+        # Quedarse con el porcentaje de fitness mayor
+        n_sobrevivientes = mutaciones['n_sobrevivientes']
+        sobrevivientes = genomas[:n_sobrevivientes]
+        self.sobrevivientes = sobrevivientes
+
+
+        # Reproducirlos mutando los pesos solamente
+            # En la reproducción, trato igual a los dos genomas (proponer y aceptar) -> simplemente hago lo mismo con ambos.
+        pesos_mutados = []
+        for i in range(mutaciones['n_mutar_pesos']):
+            genes = random.choice(sobrevivientes)
+            gen_proponer = genes[0].copy().alter_weights(mutaciones['p_mutar_peso'], mutaciones['p_gaussiana'])
+            gen_aceptar = genes[1].copy().alter_weights(mutaciones['p_mutar_peso'], mutaciones['p_gaussiana'])
+            pesos_mutados.append((gen_proponer, gen_aceptar))
+        
+        nodos_agregados = []
+        for i in range(mutaciones['n_nuevo_nodo']):
+            # elige al azar entre proponer y aceptar
+            genes = random.choice(sobrevivientes)
+            gen_proponer = genes[0].copy()
+            gen_aceptar = genes[1].copy()
+            if random.random() > 0.5:
+                gen_proponer.add_node()
+            else:
+                gen_aceptar.add_node()
+            nodos_agregados.append((gen_proponer, gen_aceptar))
+
+
+        ejes_agregados = []
+        for i in range(mutaciones['n_nuevo_eje']):
+            # elige al azar entre proponer y aceptar
+            genes = random.choice(sobrevivientes)
+            gen_proponer = genes[0].copy()
+            gen_aceptar = genes[1].copy()
+            if random.random() > 0.5:
+                gen_proponer.add_connection()
+            else:
+                gen_aceptar.add_connection()
+            ejes_agregados.append((gen_proponer, gen_aceptar))
+
+
+        cruces = []
+        for i in range(mutaciones['n_cruces']):
+            # cruza ambos
+            genes_1 = random.choice(sobrevivientes)
+            genes_2 = random.choice(sobrevivientes)
+            gen_proponer_1 = genes_1[0].copy()
+            gen_aceptar_1 = genes_1[1].copy()
+            gen_proponer_2 = genes_2[0].copy()
+            gen_aceptar_2 = genes_2[1].copy()
+            cruce_proponer = gen_proponer_1.crossing(gen_proponer_2)
+            cruce_aceptar = gen_aceptar_1.crossing(gen_aceptar_2)
+            cruces.append((cruce_proponer, cruce_aceptar))
+        
+        
+        nuevos_genomas = sobrevivientes + pesos_mutados + nodos_agregados + ejes_agregados + cruces
+        self.genomas = nuevos_genomas
+        
+        # Reordenarlos al azar
+        # Vaciar el fitness
+        random.shuffle(nuevos_genomas)
+        self.fitness_genomas = []
+   
+        return # Devolver el fitness máximo?
+    
+    def competir(self):
+        for i in range(self.n_generaciones):
+            self.generacion(self.genomas)
+            self.reproduccion(self.mutaciones)
+            if i % 10 == 0:
+                print(f"generación {i} concluida.")
+        
+        return 
+    
---
+++ src/genome.py
@@ -0,0 +1,407 @@
+import random
+from collections import deque, defaultdict
+import math
+import numpy as np
+
+def softmax(logits):
+    x = np.array(logits)
+    e_x = np.exp(x - np.max(x))  # Subtracting the maximum value for numerical stability
+    return e_x / e_x.sum(axis=0)
+
+class World:
+    def __init__(self):
+        self.innovations = 0
+    def innovation_counter(self):
+        self.innovations += 1
+        return self.innovations
+
+class Genome:
+    def __init(self, world):
+        self.nodes = {}
+        self.connections = []
+
+    def __init__(self, world, n_input, n_output):
+        self.nodes = {}  # Dictionary where key is id and value is a dictionary with bias and type
+        self.connections = []  # List of dictionaries with connection info
+        self.world = world
+
+        self.n_input = n_input 
+        self.n_output = n_output
+
+        # Create input nodes
+        for i in range(n_input):
+            self.nodes[i] = {'bias': 0, 'type': 0, 'activation' :0}
+
+        # Create output nodes
+        for i in range(n_output):
+            self.nodes[n_input + i] = {'bias': 0, 'type': 2, 'activation': 0}
+
+        # Create connections between input and output nodes
+        for i_node in range(n_input):
+            for o_node in range(n_output):
+                self.connections.append({
+                    'in': i_node,
+                    'out': n_input + o_node,
+                    'weight': random.uniform(-1, 1),
+                    'enabled': True,
+                    'innovation': world.innovation_counter()
+                })
+    def has_cycles(self, connections):
+        # Build the graph
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+
+        for connection in connections:
+            graph[connection['in']].append(connection['out'])
+            in_degree[connection['out']] += 1
+            if connection['in'] not in in_degree:
+                in_degree[connection['in']] = 0
+        
+        # Queue for nodes with no incoming edges
+        queue = deque([node_id for node_id in self.nodes if in_degree[node_id] == 0])
+        topo_sort = []
+
+        while queue:
+            node = queue.popleft()
+            topo_sort.append(node)
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        # Check if there was a cycle
+        if len(topo_sort) != len(self.nodes):
+            return True
+
+        return False
+
+    def topological_order(self):
+        # Build the graph
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+        for connection in self.connections:
+            graph[connection['in']].append(connection['out'])
+            in_degree[connection['out']] += 1
+            if connection['in'] not in in_degree:
+                in_degree[connection['in']] = 0
+        
+        # Queue for nodes with no incoming edges
+        queue = deque([node_id for node_id in self.nodes if in_degree[node_id] == 0])
+        topo_sort = []
+
+        while queue:
+            node = queue.popleft()
+            topo_sort.append(node)
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        # Check if there was a cycle
+        if len(topo_sort) != len(self.nodes):
+            print(topo_sort)
+            print(len(self.nodes))
+            for key, value in self.nodes.items():
+                print(key)
+            for connection in self.connections:
+                print(f"{connection['in']}, {connection['out']}")
+            raise ValueError("The graph has at least one cycle")
+
+        return topo_sort
+
+    def apply_activation(self, node_id):
+        node = self.nodes[node_id]
+        activation = node['bias']
+        
+        for connection in self.connections:
+            if connection['out'] == node_id and connection['enabled']:
+                input_node = self.nodes[connection['in']]
+                activation += input_node['activation'] * connection['weight']
+
+        return activation
+
+    def apply_network(self, inputValues):
+        topo_sort = self.topological_order()
+        inputs_assigned = 0
+        for i in range(len(inputValues)):
+            node = self.nodes[i]
+            if(node['type'] == 0):
+                node['activation'] = inputValues[i]
+                inputs_assigned += 1
+        if inputs_assigned != len(inputValues):
+            raise ValueError(f"Different dimensions of first layer and input vector")
+        
+        for node_id in topo_sort:
+            node = self.nodes[node_id] # revisar
+            if(node['type'] == 1): # hidden
+                activation = self.apply_activation(node_id)
+                activation = math.tanh(activation)
+                node['activation'] = activation
+
+            if(node['type'] == 2): # output
+                activation = self.apply_activation(node_id)
+                node['activation'] = activation
+        
+        last_layer_activations = []
+
+        for node_id, node in self.nodes.items():
+            if(node['type'] == 2): # Esto asume que van a seguir en orden
+                last_layer_activations.append(node['activation'])
+        
+        
+        probabilities = softmax(last_layer_activations)
+        # para sensores, no aplicar no linealidad
+        # aplicar softmax a outputs
+        # seguir orden topológico (se puede ahorrar tiempo sacando sensores y outputs en ese orden)
+        return probabilities
+    
+
+    def apply_mutation(self, p_gaussian, weight):
+        if(random.random() < p_gaussian):
+            new_weight = weight + random.gauss(0, 0.25)
+            # print(f"changed from {weight} to {new_weight}")
+            return new_weight
+        else:
+            new_weight = weight + random.uniform(-1, 1)
+            # print(f"changed from {weight} to {new_weight}")
+            return new_weight
+
+    def alter_weights(self, p_mutation, p_gaussian):
+
+        for node_id, node in self.nodes.items():
+            if(node['type'] != 0 and random.random()<p_mutation):
+                node['bias'] = self.apply_mutation(p_gaussian, node['bias'])
+        for connection in self.connections:
+            if(random.random()<p_mutation and connection['enabled']):
+                connection['weight'] = self.apply_mutation(p_gaussian, connection['weight'])
+        
+        return self
+    
+    def add_node(self):
+        connection = random.choice(self.connections)
+        while(not connection['enabled']):
+            connection = random.choice(self.connections)
+        
+        connection['enabled'] = False
+
+        new_node_id = len(self.nodes)
+
+        self.nodes[new_node_id] = {
+            'bias' : 0,
+            'type' : 1,
+            'activation' : 0
+        } 
+
+        in_connection = {
+            'in' : connection['in'],
+            'out' : new_node_id,
+            'weight' : 1,
+            'innovation' : self.world.innovation_counter(),
+            'enabled' : True
+        }
+
+        out_connection = {
+            'in' : new_node_id,
+            'out' : connection['out'],
+            'weight' : connection['weight'],
+            'innovation' : self.world.innovation_counter(),
+            'enabled' : True
+        }
+
+        self.connections.append(in_connection)
+        self.connections.append(out_connection)
+
+        # Agarrar conexión
+        # solo entre conexiones enabled
+        # Del nodo de entrada, eje con peso 1 al nodo nuevo
+        # Del nodo nuevo, eje con peso conexión vieja al nodo de salida
+        # Deshabilitar conexión vieja
+        return new_node_id
+
+    def new_connection_valid(self, in_id, out_id):
+        if in_id == out_id:
+            return False
+        if self.nodes[in_id] == None or self.nodes[out_id] == None:
+            print(f"no {in_id} or {out_id}")
+            return False
+
+        if self.nodes[out_id]['type'] == 0:
+                return False
+        for connection in self.connections:
+            if connection['in'] == in_id and connection['out'] == out_id:
+                return False
+            
+        
+        proxy_connection = {
+            'in' : in_id,
+            'out' : out_id
+        }
+        connections_2 = self.connections.copy()
+        connections_2.append(proxy_connection)
+        # print(f"checking cycles between {in_id} and {out_id}")
+        return not self.has_cycles(connections_2)
+
+    def add_connection(self):
+        highest_id = len(self.nodes) - 1
+        node1_id = random.randint(0,highest_id)
+        node2_id = random.randint(0, highest_id)
+        trials = 0
+        max_trials = 100
+        while not self.new_connection_valid(node1_id, node2_id):
+            node1_id = random.randint(0,highest_id)
+            node2_id = random.randint(0, highest_id)     
+            trials += 1  
+            if trials > max_trials:
+                return False
+
+        new_connection = {
+            'in' : node1_id,
+            'out' : node2_id,
+            'weight' : random.uniform(-1,1),
+            'enabled' : True,
+            'innovation' : self.world.innovation_counter()
+        }
+
+        self.connections.append(new_connection)
+        return new_connection['innovation']
+    
+    def crossing(self, other):
+        my_connections = self.connections 
+        others_connections = other.connections
+        i = 0
+        j = 0
+        cross_connections = []
+        new_nodes = {}
+        while i < len(my_connections) and j < len(others_connections):
+            my_connection = my_connections[i]
+            my_innovation = my_connection['innovation']
+            other_innovation = others_connections[j]['innovation']
+            if my_innovation == other_innovation:
+                if(random.random() > 0.5):
+                    cross_connections.append(my_connection.copy())
+                    my_in_node = self.nodes[my_connection['in']].copy()
+                    my_out_node = self.nodes[my_connection['out']].copy()
+                    new_nodes[my_connection['in']] = my_in_node
+                    new_nodes[my_connection['out']] = my_out_node
+                    
+                else:
+                    other_connection = others_connections[j].copy()
+                    cross_connections.append(other_connection)
+                    in_id = other_connection['in']
+                    in_node = other.nodes[in_id].copy()
+                    new_nodes[in_id] = in_node
+
+                    out_id = other_connection['out']
+                    out_node = other.nodes[out_id].copy() 
+                    new_nodes[out_id] = out_node
+
+                i+=1
+                j+=1
+
+            if my_innovation < other_innovation:
+                cross_connections.append(my_connection.copy())
+                i += 1
+                my_in_node = self.nodes[my_connection['in']].copy()
+                my_out_node = self.nodes[my_connection['out']].copy()
+                new_nodes[my_connection['in']] = my_in_node
+                
+                new_nodes[my_connection['out']] = my_out_node
+            if my_innovation > other_innovation:
+                j += 1
+        
+        while i < len(my_connections):
+            my_connection = my_connections[i].copy()
+            cross_connections.append(my_connection)
+            my_in_node = self.nodes[my_connection['in']].copy()
+            my_out_node = self.nodes[my_connection['out']].copy()
+            new_nodes[my_connection['in']] = my_in_node
+                
+            new_nodes[my_connection['out']] = my_out_node
+            i += 1
+        # align innovations
+        # choose alleles randomly from matching genes
+        # get excess and disjoint from network with higher fitness 
+        new_genome = Genome(self.world, 0, 0)
+        new_genome.nodes = new_nodes
+        """
+        for connection in cross_connections:
+            new_genome.nodes[key] = {
+                'bias' : value['bias'],
+                'type' : value['type'],
+                'activation' : value['activation']
+            }
+        """
+        if not self.has_cycles(cross_connections):
+            new_genome.connections = cross_connections
+        else:
+            new_genome.connections = self.connections
+        
+        return new_genome
+    
+    def genetic_distance(self, other, c1, c2, c3):
+        
+        excess_genes = 0
+        disjoint_genes = 0
+        matching_genes = 0
+        weight_difference_sum = 0
+        
+        genome_size = len(self.connections) + len(other.connections)
+
+        i = 0
+        j = 0
+        first_matched_gene = float('inf')
+        sorted_self = sorted(self.connections, key=lambda x:x['innovation'])
+        sorted_other = sorted(other.connections, key=lambda x: x['innovation'])
+        # print(len(sorted_self))
+        # print(len(sorted_other))
+        while i < len(sorted_self) or j < len(sorted_other):
+            # print(i)
+            # print(j)
+            if(i >= len(sorted_self) or j >= len(sorted_other) and not (i >= len(sorted_self) and j >= len(sorted_other))):
+                # print(i >= len(sorted_self))
+                # print(j >= len(sorted_other))
+                excess_genes += 1
+                i +=1
+                j +=1
+                # print("added weird excess gene")
+                continue
+            
+            my_innovation = sorted_self[i]['innovation']
+            other_innovation = sorted_other[j]['innovation']
+            if my_innovation == other_innovation:
+                
+                if first_matched_gene == float('inf'):
+                    first_matched_gene = my_innovation
+                matching_genes += 1
+                weight_difference_sum += abs(sorted_self[i]['weight'] - sorted_other[j]['weight'])
+                i+=1
+                j+=1
+                
+            elif my_innovation < other_innovation:
+                if my_innovation < first_matched_gene:
+                    excess_genes += 1
+                else:
+                    disjoint_genes += 1
+                i+= 1
+            else:
+                if other_innovation < my_innovation:
+                    if other_innovation < first_matched_gene:
+                        excess_genes += 1
+                    else:
+                        disjoint_genes += 1
+                j += 1
+        
+        # print(excess_genes)
+        # print(disjoint_genes)
+        # print(weight_difference_sum)
+
+        return c1*excess_genes/genome_size + c2*disjoint_genes/genome_size + c3*weight_difference_sum/matching_genes
+    
+    def copy(self):
+        genoma2 = Genome(self.world, 0, 0)
+        genoma2.nodes = {}
+        for key, value in self.nodes.items():
+            genoma2.nodes[key] = value.copy()
+        for connection in self.connections:
+            genoma2.connections.append(connection.copy())
+        return genoma2
